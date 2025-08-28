d7d08afe184e7d9cb3f285c3ea52cc14c413f135
SA <hm@izz.cc>
2025-08-28 19:55:06 +0800

    feat: Release v2.5.4 build 202508281951 - enhanced App Tracking Transparency implementation for App Store

--- ios/Runner/ASClassKit/ASGlobal/ASConfigConst.m
+++ ios/Runner/ASClassKit/ASGlobal/ASConfigConst.m
@@ -20,48 +20,12 @@
 
 - (void)setServerType:(kServerType)serverType {
     _serverType = serverType;
-//    self.bundleID = [[NSBundle mainBundle] bundleIdentifier];
-    self.bundleID = @"com.aoyou.xinbo";
-#ifdef DEBUG
-    switch (serverType) {
-        case kServerTest:
-        {
-            self.server_url = @"http://testoeglngldnzfgs.ixyys.cn/api/";
-            self.server_image_url = @"http://testasset.iyuanshen.cn/";
-            self.server_h5_url = @"http://testh5.ixyys.cn/";
-            self.server_im_env = @"test";
-            self.server_name = @"测试环境";
-        }
-            break;
-        case kServerPre:
-        {
-            self.server_url = @"https://preapi.ixyys.cn/api/";
-            self.server_image_url = @"https://asset.ixyys.cn/";
-            self.server_h5_url = @"https://h5.ixyys.cn/";
-            self.server_im_env = @"beta";
-            self.server_name = @"预发环境";
-        }
-            break;
-        case kServerPublish:
-        {
-            self.server_url = @"https://api.xlxban.cn/api/";
-            self.server_image_url = @"https://asset.xlxban.cn/";
-            self.server_h5_url = @"https://xlxbh5.xlxban.cn/";
-            self.server_im_env = @"";
-            self.server_name = @"线上环境";
-        }
-            break;
-        default:
-            break;
-    }
-#else
+    self.bundleID = [[NSBundle mainBundle] bundleIdentifier];
     self.NEIM_apns = NEIM_APNS_Rel;
     self.server_url = @"https://api.xlxban.cn/api/";
     self.server_image_url = @"https://asset.xlxban.cn/";
     self.server_h5_url = @"https://xlxbh5.xlxban.cn/";
     self.server_im_env = @"";
     self.server_name = @"线上环境";
-#endif
-
 }
 @end
